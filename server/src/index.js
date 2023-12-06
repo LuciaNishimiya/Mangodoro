99992bbdf83908de0b4df9471d12ce3b13167d56
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-06 07:21:19 -0300

    Basic timer to test frontend

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import cros from 'cors'
+import { create } from 'timrjs'
+const timer = create('10:00');
 const port = process.env.PORT || 3000;
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const timers = new Map();
+io.on('connection', async (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    timer.ticker(({ raw }) => {
+        socket.emit('timer', { seconds: raw.ss, minutes: raw.mm, status: 'work' });
+    });
 
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
+});
+server.listen(port, () => {
     console.log(`Server on port ${port}`);
-})
+});
+
+timer.start()
