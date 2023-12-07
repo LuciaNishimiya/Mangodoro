1aec2873e657294c8c432f0d7028e4b503a08918
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-07 16:00:07 -0300

    Add event create room and timers

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,46 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import { create } from 'timrjs'
-const timer = create('10:00');
+import { roomState } from './Services/RoomData/index.js';
+import { createTimer } from './Services/Timer/index.js';
+
 const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+io.on('connection', (socket) => {
+    console.log(`Cliente conectado: ${socket.id}`);
 
-const timers = new Map();
-io.on('connection', async (socket) => {
-    console.log(`Client connected: ${socket.id}`);
+    socket.on('createRoom', async (settings) => {
+        console.log(settings)
+        if (!roomState.has(settings.roomId)) {
+            const timer = createTimer({ settings, roomState });
+            timer.ticker(({ raw }) => {
+                io.to(settings.roomId).emit('timer',
+                    {
+                        seconds: raw.ss,
+                        minutes: raw.MM,
+                        status: timer.pomodoroStatus,
+                        owner: settings.username
+                    });
+            });
 
-    timer.ticker(({ raw }) => {
-        socket.emit('timer', { seconds: raw.ss, minutes: raw.mm, status: 'work' });
+            // Store the timer
+            roomState.set(settings.roomId,
+                {
+                    timer,
+                    owner: settings.username
+                });
+        }
+
+        socket.join(settings.roomId);
+        console.log(`Socket ${socket.id} joined room ${settings.roomId}`);
+
+        // If the room already exists, start the timer
+        if (roomState.get(settings.roomId).timer) {
+            roomState.get(settings.roomId).timer.start();
+        }
     });
-
 
     socket.on('disconnect', () => {
         console.log(`Client disconnected: ${socket.id}`);
@@ -24,5 +49,3 @@ io.on('connection', async (socket) => {
 server.listen(port, () => {
     console.log(`Server on port ${port}`);
 });
-
-timer.start()
