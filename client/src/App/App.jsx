7e67b2a47747d04505f262613cdf8680a6c544e8
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-07 15:54:33 -0300

    fix styles and import modules

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -7,12 +7,17 @@ import { Status } from '../Components/Status'
 import './App.css'
 import { useContext } from "react";
 import { PomodoroContext } from "../Context"
+
+import { Modal } from '../Components/ModalMenu'
 function App() {
   const { timer } = useContext(PomodoroContext);
   const { minutes, seconds, status } = timer
 
   return (
     <>
+      <Modal title='Settings'>
+
+      </Modal>
       <NavBar />
       <Layout>
         <Status status={status} />
