b48ee5939d19ced3835ca21e80e90c321ccb0a75
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-08 00:07:46 -0300

    Fix counter errors does not last a whole round

--- server/src/Services/Timer/index.js
+++ server/src/Services/Timer/index.js
@@ -5,25 +5,28 @@ export function createTimer({ settings, roomState, io }) {
     let timer = create(`${workTime}:00`);
     let pomodoroRounds = { total: rounds, current: rounds };
     timer.pomodoroStatus = 'work';
+    function emitTimerEvent(raw) {
+        io.to(settings.roomId).emit('timer',
+            {
+                seconds: raw.ss,
+                minutes: raw.MM,
+                status: timer.pomodoroStatus,
+                rounds: pomodoroRounds,
+                owner: settings.username,
+            });
+    }
     function start() {
         // send Timer data to users subscribed to the room
         timer.ticker(({ raw }) => {
-            io.to(settings.roomId).emit('timer',
-                {
-                    seconds: raw.ss,
-                    minutes: raw.MM,
-                    status: timer.pomodoroStatus,
-                    rounds: pomodoroRounds,
-                    owner: settings.username,
-                });
+            emitTimerEvent(raw)
             //If there are 0 seconds left on the timer, change the status and start a new counter
             if (!raw.SS) {
                 timer.stop();
-                pomodoroRounds.current--
-                if (pomodoroRounds.current >= 0) {
+                if (pomodoroRounds.current >= 1) {
                     startNewTimer()
                 } else {
                     timer.pomodoroStatus = 'finished';
+                    emitTimerEvent(raw)
                 }
             }
         });
@@ -42,6 +45,7 @@ export function createTimer({ settings, roomState, io }) {
             timer.pomodoroStatus = 'work';
             start()
             timer.start();
+            pomodoroRounds.current--
         }
     }
 
