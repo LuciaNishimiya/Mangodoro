0766ed1dad9a7c58e3edbed8f3476cdf5abf0af5
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-14 04:36:23 -0300

    improve modal menu and change context

--- client/src/Components/ModalMenu/index.jsx
+++ client/src/Components/ModalMenu/index.jsx
@@ -1,15 +1,15 @@
 import './styles.css'
 import { CloseIcon } from "../Icons"
 import { useContext } from 'react';
-import { PomodoroContext } from '../../Context';
+import { ModalContext } from '../../Context/Modal';
 export const Modal = () => {
-    const { ModalContent, setModalContent } = useContext(PomodoroContext);
+    const { ModalContent, setModalContent } = useContext(ModalContext);
     if (ModalContent) return (
 
         <div className="ModalMenu">
             <div className="ModalMenuHeader">
                 <h2>{ModalContent.title}</h2>
-                <button onClick={() => setModalContent(false)}>
+                <button onClick={() => { setModalContent(false) }}>
                     <CloseIcon />
                 </button>
             </div>
