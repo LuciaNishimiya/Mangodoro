7f7526f098cfd859099fb9a876a9c5fcbd6ea456
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-14 04:29:24 -0300

    Change Providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App'
-import { PomodoroProvider } from './Context'
+import { AppErrorProvider } from './Context/AppErrors'
+import { ModalProvider } from './Context/Modal'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <PomodoroProvider>
+    <AppErrorProvider>
+      <ModalProvider>
     <App />
-    </PomodoroProvider>
+      </ModalProvider>
+    </AppErrorProvider>
   </React.StrictMode>,
 )
