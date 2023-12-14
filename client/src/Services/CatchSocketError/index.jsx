6f2f76357040ab2a10c23edec763394b81dd7a41
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-14 05:29:22 -0300

    Add socket connection error messages

--- client/src/Services/CatchSocketError/index.jsx
+++ client/src/Services/CatchSocketError/index.jsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import { socket } from "../Socket";
+import { enqueueSnackbar } from "notistack";
 
 export const CatchSocketError = ({ setError }) => {
     useEffect(() => {
         socket.on('error', (data) => {
             setError(data);
+        });
+        socket.on('connect_error', () => {
+            enqueueSnackbar('Connection error', {
+                variant: 'error'
+            })
         });
         return () => {
             socket.off('error');
