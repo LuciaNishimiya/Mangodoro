e69224497001c6b4f866ef889a736e3b98a3924f
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-14 11:48:10 -0300

    Add pause, stop, start and next functions

--- server/src/Services/Timer/index.js
+++ server/src/Services/Timer/index.js
@@ -5,6 +5,8 @@ export function createTimer({ settings, roomState, io }) {
     let timer = create(`${workTime}:00`);
     let pomodoroRounds = { total: rounds, current: rounds };
     timer.pomodoroStatus = 'work';
+
+    // Function to emit timer events to clients
     function emitTimerEvent(raw) {
         io.to(settings.roomId).emit('timer',
             {
@@ -30,17 +32,33 @@ export function createTimer({ settings, roomState, io }) {
                 }
             }
         });
+
+        // Handle pause event
+        timer.onPause(() => {
+            io.to(settings.roomId).emit('timer',
+                {
+                    seconds: timer.getRaw().ss,
+                    minutes: timer.getRaw().MM,
+                    status: 'pause',
+                    rounds: pomodoroRounds,
+                    owner: settings.username,
+                });
+        })
     }
     // Start main timer
     start()
     // Exchange states and reset timers
     function startNewTimer() {
         if (timer.pomodoroStatus === 'work') {
+            timer.stop();
+            // Create a new timer with break time
             timer = create(`${breakTime}:00`);
             timer.pomodoroStatus = 'break';
             start()
             timer.start();
         } else if (timer.pomodoroStatus === 'break') {
+            timer.stop();
+            // Create a new timer with work time
             timer = create(`${workTime}:00`);
             timer.pomodoroStatus = 'work';
             start()
@@ -48,6 +66,36 @@ export function createTimer({ settings, roomState, io }) {
             pomodoroRounds.current--
         }
     }
+    // Function to handle the next
+    function next() {
+        if (pomodoroRounds.current >= 1) {
+            startNewTimer()
+        } else {
+            timer.pomodoroStatus = 'finished';
+            timer.stop();
+            emitTimerEvent({ ss: 0, MM: 0, })
+        }
+    }
 
+    function play() {
+        if (timer.status === 'stopped' || timer.pomodoroStatus === 'finished') {
+            // Reset rounds counter and set status to 'work'
+            pomodoroRounds = { total: rounds, current: rounds }
+            timer.pomodoroStatus = 'work';
+            start()
+        }
+        timer.start()
+    }
+    function pause() {
+        timer.pause();
+    }
+    function stop() {
+        timer.stop();
+        timer.pomodoroStatus = 'stop'
+        pomodoroRounds = { total: rounds, current: rounds }
+        emitTimerEvent({ ss: 0, MM: 0, })
+    }
+
+    timer.controls = { play, next, pause, stop }
     return timer;
 }
