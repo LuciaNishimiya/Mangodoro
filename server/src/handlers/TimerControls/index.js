fb8f0b3005a490c61069e8b68dafe45ead272510
LuciaNishimiya <lucianishimiya@gmail.com>
2024-03-01 17:57:13 -0300

    Include error codes in controls

--- server/src/handlers/TimerControls/index.js
+++ server/src/handlers/TimerControls/index.js
@@ -2,26 +2,33 @@ export function timerControls({ socket, roomState }) {
     socket.on('timerControls', async (control) => {
         const room = roomState.get(control.roomId);
 
-        if (room && room.admins.includes(control.username)) {
-            const timerControls = room.timer.controls;
+        if (room){
 
-            switch (control.action) {
-                case 'pause':
-                    timerControls.pause();
-                    break;
-                case 'stop':
-                    timerControls.stop();
-                    break;
-                case 'start':
-                    timerControls.play();
-                    break;
-                case 'next':
-                    timerControls.next();
-                    break;
-                default:
-                    socket.emit('error', { error: 'The room does not exist', code: 404 })
-                    break;
+            if (room.admins.includes(control.username)) {
+                const timerControls = room.timer.controls;
+
+                switch (control.action) {
+                    case 'pause':
+                        timerControls.pause();
+                        break;
+                    case 'stop':
+                        timerControls.stop();
+                        break;
+                    case 'start':
+                        timerControls.play();
+                        break;
+                    case 'next':
+                        timerControls.next();
+                        break;
+                    default:
+                        socket.emit('error', { error: 'The control command is incorrect', code: 405 })
+                        break;
+                }
+            } else {
+                socket.emit('error', { error: 'You do not have permissions to use the controls', code: 401 })
             }
+        } else {
+            socket.emit('error', { error: "You can't use the controls because you are not in a room", code: 400 })
         }
     });
 
