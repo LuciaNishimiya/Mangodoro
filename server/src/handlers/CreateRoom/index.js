faa2e9af176ca1393e423d1a1160f4c238911d88
LuciaNishimiya <lucianishimiya@gmail.com>
2024-03-20 01:59:57 -0300

    Make rooms reusable and restartable

--- server/src/handlers/CreateRoom/index.js
+++ server/src/handlers/CreateRoom/index.js
@@ -1,27 +1,34 @@
 import { createTimer } from '../../Services/Timer/index.js';
 export function createRoom({ socket, roomState, io }) {
     socket.on('createRoom', async (settings) => {
-        console.log('createRoom', settings);
-        // If the room does not exist, create a timer
-        if (!roomState.has(settings.roomId)) {
-            const timer = createTimer({ settings, roomState, io });
-            // Store the timer
-            roomState.set(
-                settings.roomId,
-                {
-                    timer,
-                    owner: settings.username,
-                    admins: [settings.username]
-                });
+        const timer = createTimer({ settings, roomState, io });
+        function createNewRoom(){
+            const newRoom = {
+                timer,
+                owner: settings.username,
+                admins: [settings.username]
+            };
+            roomState.set(settings.roomId, newRoom);
             console.log(`Timer created for room ${settings.roomId}`);
+            roomState.get(settings.roomId).timer.start();   
+            socket.join(settings.roomId); 
         }
 
-        socket.join(settings.roomId);
-        console.log(`Socket ${socket.id} User ${settings.username} joined room ${settings.roomId}`);
-        // If the room already exists, start the timer
-        if (roomState.get(settings.roomId).timer) {
-            roomState.get(settings.roomId).timer.start();
-            console.log(`Timer started for room ${settings.roomId}`);
+        if (roomState.has(settings.roomId)) {
+            const room = roomState.get(settings.roomId);
+            if (io.sockets.adapter.rooms.get(settings.roomId)?.size){
+                if(room.admins.includes(settings.username)){
+                    room.timer.controls.stop();
+                    room.timer = timer;
+                    room.timer.start();
+                } else {
+                    socket.emit('error', { error: 'This room already exists', code: 409 })
+                }
+            } else { 
+                createNewRoom()
+            }
+        } else { 
+            createNewRoom()
         }
     });
 };
