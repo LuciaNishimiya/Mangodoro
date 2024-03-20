b8559e005d89640c5613384bc2e68e86a02a5871
LuciaNishimiya <lucianishimiya@gmail.com>
2024-03-20 02:33:43 -0300

    improve check room function

--- server/src/handlers/CheckRoom/index.js
+++ server/src/handlers/CheckRoom/index.js
@@ -1,7 +1,17 @@
 export function checkRoom({ socket, roomState }) {
     // Check if a room exists
     socket.on('checkRoom', async (roomId) => {
-        if (!roomState.has(roomId)) {
+
+        if (roomState.has(roomId)) {
+            const room = roomState.get(settings.roomId);
+            socket.emit('checkRoom', { 
+                exist: true,
+                status: room.pomodoroStatus, 
+                owner: room.owner,
+                admins: room.admins,
+            });
+        } 
+        else {
             socket.emit('error', { error: 'The room does not exist', code: 404 })
         }
     });
